23889ea0a12478937ea510539b84496c74d13592
Hans <hans@myre.life>
2022-10-11 16:43:33 +0800

    update photo

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import './index.scss';
 
 const index = ({ search, setInput }) => {
+  const keyDownHandler = (e) => {
+    if (e.keyCode === 13) {
+      search();
+    }
+  }
   return (
     <div className='search'>
-      <input type="text" onChange={(e) => setInput(e.target.value)}></input>
+      <input type="text" onChange={(e) => setInput(e.target.value)} onKeyDown={keyDownHandler}></input>
       <button onClick={search}>Search</button>
     </div>
   )
--- src/pages/photo/index.js
+++ src/pages/photo/index.js
@@ -4,12 +4,17 @@ import Picture from 'components/Picture';
 import './index.scss';
 
 const index = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [input, setInput] = useState('');
-  const auth = '563492ad6f917000010000014a7c8dbe0a924a789f687ada0a5bbe5e';
-  const initURL = 'https://api.pexels.com/v1/curated?per_page=10';
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=10`;
+  const [page, setPage] = useState(1);
+  const [currentSearch, setCurrentSearch] = useState('');
+  const baseURL = 'https://api.pexels.com/v1';
+  // const auth = '563492ad6f917000010000014a7c8dbe0a924a789f687ada0a5bbe5e';
+  const auth = '563492ad6f91700001000001ac22187f3df749b5ac359637c75cb9b5';
+  const initURL = `${baseURL}/curated?per_page=10&page=1`;
+  const searchURL = `${baseURL}/search?query=${currentSearch}&per_page=10&page=1`;
   const search = async(url) => {
+    if (url !== initURL && !currentSearch)  return;
     const dataFetch = await fetch(url, {
       method: 'GET',
       headers: {
@@ -19,21 +24,48 @@ const index = () => {
     })
     let parsedData = await dataFetch.json();
     setData(parsedData.photos);
+    setPage(2);
   }
+
+  const morepicture = async() => {
+    let newURL;
+    if (currentSearch === '') {
+      newURL = `${baseURL}/curated?per_page=10&page=${page}`;
+    } else {
+      newURL = `${baseURL}/search?query=${currentSearch}&per_page=10&page=${page}`;
+    }
+    setPage(page+1);
+    const dataFetch = await fetch(newURL, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: auth,
+      }
+    })
+    let parsedData = await dataFetch.json();
+    setData(data.concat(parsedData.photos));
+  }
+
   useEffect(() => {
-    if (input === '') {
+    if (currentSearch === '') {
       search(initURL);
     } else {
       search(searchURL);
     }
-  }, []);
+  }, [currentSearch]);
 
 
   return (
     <div className='photo'>
-      <Search search={() => search(searchURL)} setInput={setInput}/>
+      <Search search={() => {
+        // JS Closure
+        setCurrentSearch(input); 
+      }} setInput={setInput}/>
       <div className='pictures'>
         {data && data.map(d => <Picture data={d} key={d.id}/>)}
+      </div>
+      <div className='morePicture'>
+        <button onClick={morepicture}>Load More</button>
       </div>
     </div>
     
